39e5649cb5d2d0b7237c1c95e5be8ed0a5d9a67a
Akriti Srivastava <akisrivastava22@gmail.com>
2023-08-25 22:12:42 +0530

    working on Laptop UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Statistics from "./Components/Statistics";
 import SlidingMenu from "./Components/SlidingMenu";
 function App() {
   return (
-    <div className="lg:flex">
+    <div className="lg:flex   bg-gray-100">
       <SlidingMenu />
-      <div>
+      <div className="bg-white md:mx-4 md:rounded-xl md:max-h-[650px]">
         <Nav />
         <Statistics />
         <Assessments />
--- src/Components/SlidingMenu.jsx
+++ src/Components/SlidingMenu.jsx
@@ -11,9 +11,9 @@ const SlidingMenu = () => {
     <>
       {/* Sliding Menu for Hamburger Menu */}
       <div className="w-[100vw] lg:hidden h-full top-0 left-0 absolute bg-gray-800 opacity-50"></div>
-      <nav className="lg: relative z-10 list-none pl-3 pb-60 bg-blue-50 h-full w-80">
-        <div className="flex justify-between pt-10">
-          <li className="text-lg">Menu</li>
+      <nav className="md:bg-white z-10 list-none pl-3  md:px-3 pb-60 bg-blue-50 h-full w-80 lg:h-[100vh] md:max-w-[140px]">
+        <div className="flex md:hidden justify-between pt-10">
+          <li className="text-lg ">Menu</li>
           <button onClick={closeMenu} className="float-right mr-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -27,24 +27,24 @@ const SlidingMenu = () => {
           </button>
         </div>
         <div className="mt-8 text-xl ml-2">
-          <li className="flex gap-2 items-center ">
-            <LuLayoutDashboard />
+          <li className="flex md:text-sm md:font-semibold md:flex-col gap-2 items-center ">
+            <LuLayoutDashboard className="md:text-xl" />
             Dashboard
           </li>
-          <div className="text-blue-600 mt-5 border-2 py-2 px-2 border-blue-300 bg-blue-100 mr-5 -ml-2 rounded-lg ">
-            <li className="flex gap-2 items-center">
-              <BsClipboardCheck />
+          <div className="text-blue-600  mt-5 border-2 py-2 px-2 border-blue-300 bg-blue-100 mr-5 md:mr-0  -ml-2 rounded-lg ">
+            <li className="flex gap-2 items-center md:font-semibold md:flex-col  md:text-sm">
+              <BsClipboardCheck className="md:text-xl" />
               Assessment
             </li>
           </div>
-          <li className="flex gap-2 items-center mt-5 ">
-            <MdOutlineLiveHelp />
+          <li className="flex  md:text-sm md:flex-col md:font-semibold gap-2 items-center mt-5 ">
+            <MdOutlineLiveHelp className="md:text-xl" />
             My Library
           </li>
           <hr className=" mt-5 mr-4 border-dashed border-1 border-gray-400" />
-          <div className="flex justify-between items-center">
-            <li className="flex gap-2 items-center mt-5">
-              <PiLinkFill />
+          <div className="flex md:flex-col-reverse justify-between items-center">
+            <li className="flex  md:text-sm md:font-semibold md:flex-col gap-2 items-center mt-5">
+              <PiLinkFill className="md:text-xl" />
               Round Status
             </li>
             <button className="mt-5 rounded-full border-2 border-red-500 px-3 text-sm bg-red-100 text-red-500 mr-2">
