b300213faeac7b493694428cd0ada5401e1db84d
Akriti Srivastava <akisrivastava22@gmail.com>
2023-08-26 16:34:34 +0530

    fixed md styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Statistics from "./Components/Statistics";
 import SlidingMenu from "./Components/SlidingMenu";
 function App() {
   return (
-    <div className="lg:flex items-start justify-start  md:bg-gray-100">
-      <div className="hidden md:block md:bg-white">
+    <div className="md:flex items-start justify-start  md:bg-gray-100">
+      <div className="hidden md:inline-block md:bg-white">
         <SlidingMenu />
       </div>
       <div className="bg-white md:mx-4 md:rounded-xl md:max-h-[650px]">
